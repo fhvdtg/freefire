89b9a060821d8eb018366d723d1de77d49dafdb2
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-09-13 14:43:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3512,4 +3512,25 @@ client.on("guildMemberAdd", member => {
       
       }); 
 
+client.on("message", (message) => {
+if (message.content.startsWith("!صوت")) {
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+        let args = message.content.split(" ").slice(1);
+    message.guild.createChannel(args.join(' '), 'voice');
+message.channel.sendMessage('**تـم إنـشاء روم صوتي**')
+   
+}
+});
+ 
+ 
+client.on("message", (message) => {
+if (message.content.startsWith("!كـتاب")) {
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+        let args = message.content.split(" ").slice(1);
+    message.guild.createChannel(args.join(' '), 'text');
+message.channel.sendMessage('**تـم إنـشاء روم كـتابـي**')
+ 
+}
+});
+
 client.login(process.env.BOT_TOKEN);// دماء bot
